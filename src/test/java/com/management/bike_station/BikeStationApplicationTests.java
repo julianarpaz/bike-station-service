7a9ebac71a672046cee6e42acdbc81ee15dd6863
Juliana Ribeiro <julianarpaz@gmail.com>
2024-08-31 12:58:20 -0300

    test: adding test application

---
+++ src/test/java/com/management/bike_station/BikeStationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management.bike_station;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeStationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
